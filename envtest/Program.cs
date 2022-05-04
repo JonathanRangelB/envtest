d485e296bfaee01c6f39c8b3c1eb8194ddc14180
Jonathan Rangel Bernal <necro@Mac-mini.local>
2022-05-04 15:47:05 -0500

    initial commit

---
+++ envtest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace envtest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var temp = Environment.GetEnvironmentVariable("nombre");
+            if (temp != null)
+                Console.WriteLine(temp);
+            else Console.WriteLine("no encontrada");
+        }
+    }
+}
